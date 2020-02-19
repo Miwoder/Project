f0757255b9b778f69da52d2c1a62996fde95f464
Miwoder <bestboy75@rambler.ru>
2020-02-19 18:44:56 +0300

    rename package

--- src/com/BSTU/GovoronokVadim/CreateOrder.java
+++ src/com/BSTU/GovoronokVadim/CreateOrder.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.BSTU.GovoronokVadim;
 
 import lombok.Data;
 import java.sql.Date;
--- src/com/BSTU/GovoronokVadim/Main.java
+++ src/com/BSTU/GovoronokVadim/Main.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.BSTU.GovoronokVadim;
 
 public class Main {
 
